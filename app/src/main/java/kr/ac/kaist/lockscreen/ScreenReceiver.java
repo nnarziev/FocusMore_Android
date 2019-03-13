14e7a0443aa1f23590c4c86eb52ccaae75099c79
nnarziev95 <nnarziev@gmail.com>
2019-03-13 17:16:11 +0900

    Errors fixing

--- app/src/main/java/kr/ac/kaist/lockscreen/ScreenReceiver.java
+++ app/src/main/java/kr/ac/kaist/lockscreen/ScreenReceiver.java
@@ -8,7 +8,6 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.os.Bundle;
 import android.util.Log;
 import android.widget.Toast;
 
